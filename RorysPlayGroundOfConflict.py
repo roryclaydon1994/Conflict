6ee52834c17694a1c105417175b7657ead5e4498
Rory <rory.claydon@ed.ac.uk>
2022-08-30 10:28:02 +0100

    started to add documentation

--- RorysPlayGroundOfConflict.py
+++ RorysPlayGroundOfConflict.py
@@ -6,14 +6,21 @@ from pprint import pprint
 import matplotlib.pyplot as plt
 
 def addEventsToMap():
+    """
+    Plot events on a map
 
+    Parameters:
+
+    Returns:
+
+    """
     world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
     fig,ax=plt.subplots(1,1,figsize=[20,20])
     base = world.plot(ax=ax,color='white', edgecolor='black')
     ax.set_aspect('equal')
     ax.set_xlabel("longitude")
     ax.set_ylabel("latitude")
-    plt.show()
+    return fig,ax
 
 df=pd.read_csv("TeamConflict/conflict_data_acled_2018_2021.csv")
 print(df.columns)
